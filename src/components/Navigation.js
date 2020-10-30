38c547fbe3daefe425587b6b9c51be447947d4b0
Jinu Kim <wlsdn5153@naver.com>
2020-10-30 16:17:32 +0900

    네비게이션 진짜완료

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,18 +3,47 @@ import { Link } from 'react-router-dom';
 import './css/Navigation.css'
 
 function Navigation() {
+
+  const onClickHandler = () => {
+    const navMenu = document.querySelector('.navigation__menu');
+    const navCheck = document.querySelector('.navigation__check');
+    navMenu.classList.toggle('active');
+    navCheck.classList.toggle('active');
+  }
+
   return (
-    <div className="nav__container">
-      <div className="nav__inner">
-        <Link className="nav__logo" to="/">logo</Link>
-        <Link to="#">New</Link>
-        <Link to="#">Best</Link>
-        <Link to="#">Choice</Link>
-        <Link to="/about">About</Link>
-        <Link to="#">Support</Link>
-      </div>
-    </div>
+    <>
+      <nav className="navigation__navbar">
+        <div className="navigation__inner">
+          <div className="navigation__logo">
+            <Link className="nav__logo" to="/">
+              <i className="fas fa-carrot fa-2x"></i>
+            </Link>
+          </div>
+
+          <ul className="navigation__menu">
+            <li><Link to="#">New</Link></li>
+            <li><Link to="#">Best</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="#">Support</Link></li>
+          </ul>
+
+          <div className="navigation__check">
+            <Link to="#">Login</Link>
+            <Link to="#">Logout</Link>
+          </div>
+
+          <a href="#" className="navigation__togglebtn" onClick={onClickHandler}>
+            <i className="fas fa-bars"></i>
+          </a>
+        </div>
+      </nav>
+    </>
   )
 }
 
 export default Navigation
+
+
+
+
